ad6b9fd18caee4de1df0d0e43c6ec637ab9cf225
Álvaro <alvarofragosoc@hotmail.com>
2024-07-23 15:59:36 +0200

    healthchek on /

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use axum::{response::IntoResponse, routing::get, Json, Router};
 
 async fn health_checker_handler() -> impl IntoResponse {
     const MESSAGE: &str = "Build Simple CRUD API in Rust using Axum";
-
+    println!("<-GET 200 health_checker_handler");
     let json_response = serde_json::json!({
         "status": "success",
         "message": MESSAGE
@@ -13,9 +13,9 @@ async fn health_checker_handler() -> impl IntoResponse {
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/api/healthchecker", get(health_checker_handler));
+    let app = Router::new().route("/", get(health_checker_handler));
 
-    println!("🚀 Server started successfully");
+    println!("🚀 Server started successfully in port 8000");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
